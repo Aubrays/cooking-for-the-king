37ed51a742436714e80fdd4e2e1c87dd01575819
Saara <astonedf@users.noreply.github.com>
2019-10-17 22:39:29 +0200

    test drag n drop
    essai du code drag n drop avec un sprite

---
+++ game.js
@@ -0,0 +1,36 @@
+console.clear();
+
+var config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 600,
+    backgroundColor: '#FFFFFF',
+    // parent: 'phaser-example',
+    physics: {
+        default: 'matter'
+    },
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('leek', 'assets/sprites/leek.png');
+}
+
+function create ()
+{
+    this.matter.world.setBounds();
+
+    this.matter.add.image(100, 400, 'leek', null, { chamfer: 16 }).setBounce(0.5);
+
+    //  These both do the same thing:
+
+    // this.matter.add.pointerConstraint({ length: 1, stiffness: 0.6 });
+
+    this.matter.add.mouseSpring({ length: 1, stiffness: 0.5 });
+}
