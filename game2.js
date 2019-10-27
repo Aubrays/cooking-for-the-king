b2638cec3c8ef4a53f62ec47826abf13e434b217
RPetkovic <radisa.petkovic@unil.ch>
2019-10-28 00:24:21 +0100

    Update game2.js

--- game2.js
+++ game2.js
@@ -17,11 +17,16 @@ var game = new Phaser.Game(config)
 function preload(){
     this.load.image('leek', 'assets/sprites/leek.png');
     this.load.image('chaudron', 'assets/sprites/chaudron.png');
+    this.load.spritesheet('food', 'assets/sprites/food.png', 15, 17);
+
 }
 
 function create(){
-    this.physics.add.image(100, 300, 'leek')
-    this.physics.add.image(300, 200, 'chaudron')
+    this.add.text(300, 250, 'Loading game...', {font:"25px Arial", fill:"yellow"});
+    this.physics.add.image(100, 300, 'leek');
+    this.physics.add.image(300, 200, 'chaudron');
+    this.physics.add.spritesheet(200, 100, 'food');
+
 }
 
 function update(){
