b282ce157904efbf5ccf4ee5ad1dbeda5e3ae891
Aubrays <contact@aubrays.ch>
2019-12-16 15:02:34 +0100

    add class ProgressBar

---
+++ classes/ProgressBar.js
@@ -0,0 +1,87 @@
+class ProgressBar extends Phaser.GameObjects.Graphics {
+
+    // Sketch
+    // -4  -3  -2  -1   0   1   2   3   4
+    //  |   |   |   |   |   |   |   |   |
+    // => 9 positions, 8 intervals
+
+    constructor(scene, options) {
+        super(scene, options);
+
+        config.bar = options;
+        // config.bar.pos.x -= ( config.bar.size.w / 2 ); /// center on X axis ////
+        // config.bar.pos.y -= ( config.bar.size.h / 2 ); /// center on Y axis ////
+
+
+        this.x = config.bar.pos.x/2; // Must divided by 2 for an unknown reason...
+        this.y = config.bar.pos.y/2; // Must divided by 2 for an unknown reason...
+        this.w = config.bar.size.w;
+        this.z = config.bar.size.h;
+        this.defaultFillColor = config.bar.fill_color;
+        this.defaultFillAlpha = 0.7;
+        this.defaultStrokeColor = config.bar.border_color;
+        this.startValue = config.bar.startValue;
+        this.goalValue = config.bar.goalValue;
+        this.state = 'created';
+
+        let borderOffset = 1;
+
+        let borderRect = new Phaser.Geom.Rectangle(
+            this.x*2 - borderOffset,
+            this.y*2 - borderOffset,
+            this.w + borderOffset * 2,
+            this.z + borderOffset * 2
+        );
+
+        let border = scene.add.graphics({
+            lineStyle: {
+                width: 2,
+                color: this.defaultStrokeColor
+            }
+        });
+        border.strokeRectShape(borderRect);
+
+        // let goalStroke = new Phaser.Geom.Line(
+        //     ((this.startValue + 4) / 8) * this.x*2,
+        //     this.z*2,
+        //     ((this.startValue + 4) / 8) * this.x*2 +2,
+        //     this.y*2 + this.z
+        // );
+        let goalStroke = new Phaser.Geom.Line(
+            this.x*2,
+            this.y*2 + borderOffset,
+            this.x*2,
+            this.z + borderOffset
+        );
+
+        console.log(goalStroke);
+
+        let goal = scene.add.graphics({
+            lineStyle: {
+                color: 0x00cc00
+            }
+        });
+        goal.strokeLineShape(goalStroke);
+        goal.setDepth(100); // Same as z-index
+
+        scene.add.existing(this);
+        console.log('create x,y:' + this.x + ',' + this.y);
+    }
+
+    updateProgressBar(value){ //// health percentage
+        
+        if(this.state == 'created'){
+            value = this.startValue;
+        }
+        let percentage = (value + 4) / 8;
+
+        this.clear();
+        this.fillStyle( this.defaultFillColor, this.defaultFillAlpha );
+        this.fillRect( this.x, this.y, percentage * this.w , this.z);
+        console.log('update x,y:' + this.x + ',' + this.y);
+        this.setState('updated');
+    }
+    // ...
+
+    // preUpdate(time, delta) {}
+}
