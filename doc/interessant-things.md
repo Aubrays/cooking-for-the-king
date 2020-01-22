5b31eea30941170003b67de92c55aeab2e83dfee
Aubrays <contact@aubrays.ch>
2020-01-22 21:39:05 +0100

    Updates

--- doc/interessant-things.md
+++
@@ -1,3 +0,0 @@
-# Some interessant things
-
-- drag and drop with gravity : http://labs.phaser.io/view.html?src=src/physics/matterjs/drag%20with%20pointer.js
--- src/helpers/calcStats.js
+++
@@ -1,5 +0,0 @@
-export const getActualStat = (scene, stat) => ({
-    "heat": scene.char.data.values.healthStats.heatStart + scene.dish.heat,
-    "moistness" : scene.char.data.values.healthStats.moistnessStart + scene.dish.moistness,
-    "wealth" : scene.char.data.values.wealth - scene.dish.cost    
-})[stat]
